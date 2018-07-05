7a79fa0f34e8f7c916ef83cb50b95e6558ef643b
bojanpotocnik <info@bojanpotocnik.com>
2018-07-05 21:07:20 +0200

    Async data fetch from the page successful

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import asyncio
+import datetime
 import enum
+from typing import List, Dict, Tuple
+
+import aiohttp
 
 __author__ = "Bojan Potočnik"
 
@@ -9,5 +14,20 @@ class Restaurants(enum.Enum):
     PERLA = r"http://www.antaro.si/perla/dnevni_menu/"
 
 
+async def fetch(restaurant: Restaurants, *, loop: asyncio.AbstractEventLoop = None) \
+        -> Tuple[Restaurants, Dict[Tuple[str, datetime.date], List[str]]]:
+    async with aiohttp.ClientSession(loop=loop) as session:
+        async with session.get(restaurant.value) as response:
+            response = await response.text()
+            print(response)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+
+    result = loop.run_until_complete(fetch(Restaurants.ANTARO))
+    print(result)
+
+
 if __name__ == "__main__":
-    pass
+    main()
